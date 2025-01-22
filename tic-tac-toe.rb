b4cfef2908052d6da423cf9fdb537ddafb2230fb
khabigcorp <99237998+khabigcorp@users.noreply.github.com>
2025-01-22 18:11:40 +0800

    Made tic-tac-toe functional

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,26 +1,31 @@
+require 'pry-byebug'
+
 class Board
-  @@winning_combinations = %w[[0 1 2] [3 4 5] [6 7 8] [0 3 6] [1 4 7] [2 5 8] [0 4 8] [1 4 7]]
+  @@winning_combinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
   def initialize
-    @board_squares = %w[1 2 3 4 5 6 7 8 9]
+    @board_squares = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     @placed_squares = 0
     @has_winner = false
   end
 
   def has_winner?
-    @has_winner = @@winning_combination.any? { |index_combinations| board_squares[index_combinations[0]] == board_squares[index_combinations[1]] == board_squares[index_combinations[2]] }
+    @has_winner = @@winning_combinations.any? do |index_combinations|
+      @board_squares[index_combinations[0]] == @board_squares[index_combinations[1]] && @board_squares[index_combinations[1]] == @board_squares[index_combinations[2]]
+    end
   end
 
   def show_board
-    puts "#{board_squares[0]}|#{board_squares[1]}|#{board_squares[2]}\n" + "-----\n" + "#{board_squares[3]}|#{board_squares[4]}|#{board_squares[5]}\n" + "-----\n" + "#{board_squares[6]}|#{board_squares[7]}|#{board_squares[8]}"
+    puts "#{@board_squares[0]}|#{@board_squares[1]}|#{@board_squares[2]}\n" + "-----\n" + "#{@board_squares[3]}|#{@board_squares[4]}|#{@board_squares[5]}\n" + "-----\n" + "#{@board_squares[6]}|#{@board_squares[7]}|#{@board_squares[8]}\n"
   end
 
   def play(position, character)
-    if (position <= 0 || position > 9) return
-      
-    if board_squares[position-1].to_i != position
-      board_squares[position-1] = character
-      @placed_squares += 1
-    end
+    return false if position <= 0 || position > 9
+
+    return false unless @board_squares[position - 1] == position
+
+    @board_squares[position - 1] = character
+    @placed_squares += 1
+    true
   end
 
   def can_be_won?
@@ -33,18 +38,32 @@ class Game
     @board = Board.new
     @turn_count = 1
   end
+
   def start
+    puts 'What is your name, player 1?'
     @player1_name = gets.chomp
+    puts "\nWhat is your name, player 2?"
     @player2_name = gets.chomp
-    unless Board.has_winner? or !Board.can_be_won?
-      current_player = (turn_count % 2 == 1) ? @player1_name : @player2_name 
-      current_character = (turn_count % 2 == 1) ? 'O' : 'X'
-      puts "Where will you place your #{current_character}, #{current_player}?"
-      @board.play(position, current_character)
+    puts "\nLet the game begin!\n"
+
+    @board.show_board
+    until @board.has_winner? || !@board.can_be_won?
+      current_player = @turn_count.odd? ? @player1_name : @player2_name
+      current_character = @turn_count.odd? ? 'O' : 'X'
+      puts "\nWhere will you place your #{current_character}, #{current_player}?"
+      position = gets.chomp.to_i
+
+      next unless @board.play(position, current_character)
+
+      puts
       @board.show_board
       @turn_count += 1
+      puts
     end
 
     puts "#{current_player} has won!"
   end
 end
+
+tic_tac_toe = Game.new
+tic_tac_toe.start
